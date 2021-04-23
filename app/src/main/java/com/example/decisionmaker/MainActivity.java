3538e1741d9aa164e485655458f2705833ffb505
Marukos <mkoletsas@csd.auth.gr>
2021-04-23 17:42:11 +0300

    first commit

--- app/src/main/java/com/example/decisionmaker/MainActivity.java
+++ app/src/main/java/com/example/decisionmaker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.decisionmaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("petuxe?");
+    }
+
+    public void goToPeopleList(View view){
+        Intent intent = new Intent(this, PeopleList.class);
+        startActivity(intent);
+    }
+
+    public void goToShoppingList(View view){
+        Intent intent = new Intent(this, ShoppingList.class);
+        startActivity(intent);
+    }
+
+    public void goToActivitiesList(View view){
+        Intent intent = new Intent(this, ActivitiesList.class);
+        startActivity(intent);
+    }
+
+    public void goToTravellingList(View view){
+        Intent intent = new Intent(this, TravellingList.class);
+        startActivity(intent);
     }
 }
