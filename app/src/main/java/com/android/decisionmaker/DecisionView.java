fc8ca3fc1ad045ffeafada4032204bd2d21bdc24
GeorgeJoLo <gtzolopo@csd.auth.gr>
2021-05-15 18:12:54 +0300

    AnyChart

--- app/src/main/java/com/android/decisionmaker/DecisionView.java
+++ app/src/main/java/com/android/decisionmaker/DecisionView.java
@@ -7,6 +7,16 @@ import android.widget.TextView;
 
 import com.android.decisionmaker.database.models.Decision;
 
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.DataEntry;
+import com.anychart.anychart.Pie;
+import com.anychart.anychart.ValueDataEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DecisionView extends AppCompatActivity {
 
     TextView name;
@@ -26,6 +36,18 @@ public class DecisionView extends AppCompatActivity {
         }
 
         Decision decision = new Decision();  // -----------------------------------------------------------------
+
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
+
 
 
     }
